60fae04fde27701b21dd4d8fc8bc0c07b1f07255
runner_bot <runner@runner.com>
2024-05-14 23:38:24 +0800

    Adding test containers code

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -13,6 +13,28 @@ class DemoApplicationTests {
 
 	@Test
 	fun testcontainerstest() {
+		val container = ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:7.15.0")
+
+		container.start()
+
+		val credentialsProvider = BasicCredentialsProvider().apply {
+			setCredentials(
+				AuthScope.ANY,
+				UsernamePasswordCredentials(ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD)
+			)
+		}
+
+		val client = RestClient.builder(HttpHost.create(container.httpHostAddress))
+			.setHttpClientConfigCallback { httpClientBuilder ->
+				httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
+			}
+			.build()
+
+		val response = client.performRequest(Request("GET", "/_cluster/health"))
+
+		// Use the response as needed
+
+		container.close()
 		assertTrue(true);
 	}
 }

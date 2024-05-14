878e78c38fd9ba42cd67630167a2b88bf3d8459e
runner_bot <runner@runner.com>
2024-05-14 23:44:23 +0800

    Adding the Request library

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -3,6 +3,7 @@ package com.example.demo
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.junit.jupiter.api.Assertions.assertTrue;
+import org.apache.http.Request
 
 @SpringBootTest
 class DemoApplicationTests {
